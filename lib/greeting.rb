db33002b5b9b8dbe7ab0a9ccd585a660b303674c
Luke Lin <lukelin1991@yahoo.com>
2019-11-10 22:00:49 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code the #greeting method here!
 def greeting(name = "Sally")
   puts "Hi! I'm HAL, what's your name?"
-  #name = gets
+  name = gets.chomp
   puts "Hello #{name}. It's nice to meet you."
 end
